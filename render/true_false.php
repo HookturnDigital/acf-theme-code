06f0bb06d273e609e78c499f2a62ad0c1c90a0d2
Aaron Rutley <aaronrutley@gmail.com>
2016-08-04 11:38:13 +1000

    bug in true false

--- render/true_false.php
+++ render/true_false.php
@@ -5,7 +5,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 
-echo $this->indent . htmlspecialchars("<?php if( " . $this->get_field_method . "( '" . $this->name . "' ) == 1 ) { ")."\n";
+echo $this->indent . htmlspecialchars("<?php if ( " . $this->get_field_method . "( '" . $this->name . "' ) == 1 ) { ")."\n";
 echo $this->indent . htmlspecialchars(" // echo 'true'; ")."\n";
 echo $this->indent . htmlspecialchars("} else { ")."\n";
 echo $this->indent . htmlspecialchars(" // echo 'false'; ")."\n";
