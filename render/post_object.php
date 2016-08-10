3e326e490df97c78ecfec8c4f1b095fc55ba998a
Aaron Rutley <aaronrutley@gmail.com>
2016-08-11 00:27:59 +1000

    post ojbect fix

--- render/post_object.php
+++ render/post_object.php
@@ -12,13 +12,14 @@ $multiple_values = isset( $this->settings['multiple'] ) ? $this->settings['multi
 // NOTE: Not supported by ACF v4
 $return_format = isset( $this->settings['return_format'] ) ? $this->settings['return_format'] : '';
 
-// If ID and single
-if( $return_format == 'id' && $multiple_values == '0' ) {
+// If not object (so ID or Null) and single
+if( $return_format != 'object' && $multiple_values == '0' ) {
 	echo $this->indent . htmlspecialchars("<?php \$".$this->name. ' = ' . $this->get_field_method . "( '" . $this->name ."' ); ?>")."\n";
+	echo $this->indent . htmlspecialchars("<?php // var_dump( \$".$this->name." ); ?>")."\n";
 }
 
-// If ID and multiple
-if( $return_format == 'id' && $multiple_values == '1' ) {
+// If not object (so ID or Null) and multiple
+if( $return_format != 'object' && $multiple_values == '1' ) {
 	echo $this->indent . htmlspecialchars("<?php \$".$this->name.' = ' . $this->get_field_method . "( '" . $this->name ."' ); ?>")."\n";
 	echo $this->indent . htmlspecialchars("<?php // var_dump( \$".$this->name." ); ?>")."\n";
 }
