c9185cd2817ca6aeab5504213938002eea4ee121
Ben Pearson <ben@benpearson.com.au>
2020-04-16 12:05:26 +1000

    Updates to locations

--- core/locations.php
+++ core/locations.php
@@ -16,13 +16,22 @@ class ACFTC_Locations {
 	// Locations that are excluded because they aren't really locations
 	// (they relate to the backend visiblity of the field group)
 	private static $locations_excluded = array(
-		// ACF v5
-		'current_user',
-		'current_user_role',
-		'user_role',
 		// ACF v4
 		'user_type', // Logged in User Type
 		'ef_user' // User
+	);
+
+	private static $locations_tc_pro = array(
+		'current_user',
+		'current_user_role',
+		'user_form',
+		'user_role',
+		'attachment',
+		'taxonomy',
+		'comment',
+		'widget',
+		'block',
+		'options_page',
 	);
 
 	/**
@@ -181,7 +190,7 @@ class ACFTC_Locations {
 
 		$args = array(
 			'field_group_id' => $this->field_group_post_ID
-			// no location argument included
+			// no location argument included here, only used below
 		);
 		$parent_field_group= new ACFTC_Group( $args );
 
@@ -198,46 +207,191 @@ class ACFTC_Locations {
 			<?php return ob_get_clean();
 		}
 
-		ob_start();
-
-		// If more than one location: render upgrade notice
-		if ( count( $this->location_rules) > 1 ) {
-			echo $this->get_locations_upgrade_notice_html();
+		// If all locations are excluded: render fields without location ui
+		// elements (eg. only the Current User location is selected)
+		if ( empty( $this->location_rules ) ) {
+			return $parent_field_group->get_field_group_html();
 		}
 
-        echo $parent_field_group->get_field_group_html(); 
+		ob_start();
 
-        echo $this->get_after_field_group_upgrade_notice_html();
-				
+		// If more than one location: render location select
+		if ( count( $this->location_rules) > 1 ) {
+			echo $this->get_location_select_html();
+		}
+
+		// Render ALL fields for every location
+		foreach ( $this->location_rules as $index => $location_rule ) :
+
+			echo $this->get_single_location_html( $location_rule, $index );
+
+		endforeach;
+
+        echo $this->get_after_field_group_notice_html();
+
 		return ob_get_clean();
 
 	}
 
 
+
 	/**
-	 * Render upgrade link for location support
+	 * Get location name in a clean title case format
+	 * for use in location select and titles
+	 * 
+	 * @param Array $location_rule
+	 * Array (
+	 *	[param] => block
+	 *	[operator] => ==
+	 *	[value] => acf/example
+	 * ) 
+	 * @return string 
+	 * 
+	 */
+	private function get_location_clean_text( $location_rule ) { 
+
+		if ( !$location_rule ) {
+			return;
+		}
+
+		$location_name_title_case = ucwords( str_replace('_', ' ', $location_rule['param'] ) );
+
+		if ( empty( $location_rule['value'] ) ) {
+			$location_value_no_dashes = 'unknown';
+		} else {
+			$location_value_no_dashes = str_replace( '-' , ' ', $location_rule['value'] );
+		}
+		
+		$location_value_clean = str_replace( array( 'category:', 'acf/' ) , '', $location_value_no_dashes );
+		$location_value_title_case = ucwords( $location_value_clean );
+
+		$location_operator_text = ($location_rule['operator'] === '==' ) ? '' : 'Not ';
+
+		return "{$location_name_title_case} ({$location_operator_text}{$location_value_title_case})";
+
+	}
+
+
+	/**
+	 * Render header for location select
 	 * 
 	 * @return string 
 	 */
-	private function get_locations_upgrade_notice_html() { 
+	private function get_location_select_html() { 
 		
 		ob_start();
-        ?>
-<div class="acftc-pro-notice acftc-pro-notice--top">
-    <a class="acftc-pro-notice__link" href="https://hookturn.io/downloads/acf-theme-code-pro/?utm_source=acftclocation" target="_blank">Generate code for each of your Location rules with <strong>ACF Theme Code Pro</strong></a>
+		?>
+<div class="inside acf-fields -left acf-locations">
+	<div class="acf-field acf-field-select" data-name="style" data-type="select">
+		<div class="acf-label">
+			<label for="acf_field_group-style">Location</label>
+			<p class="description">Select a location to see the relevant theme code</p>
+		</div>
+		<div class="acf-input">
+			<select id="acftc-group-option" class="" data-ui="0" data-ajax="0" data-multiple="0" data-placeholder="Select" data-allow_null="0">
+<?php foreach ( $this->location_rules as $key => $location_rule ) : ?>
+				<option value="acftc-group-<?php echo $key; ?>"><?php echo $this->get_location_clean_text( $location_rule ); ?></option>
+<?php endforeach; ?>
+			</select>
+		</div>
+	</div>
 </div>
 
 <?php 
+		return ob_get_clean(); 
+	}
+
+
+	/**
+	 * Get HTML for a single location
+	 *
+	 * @param Array $location_rule
+	 * Array (
+	 *	[param] => block
+	 *	[operator] => ==
+	 *	[value] => acf/example
+	 * ) 
+	 * @param int $index Identifier for the location
+	 * @return string 
+	**/
+	protected function get_single_location_html( $location_rule, $index ) {
+		
+		ob_start();
+
+		// Wrapping div used for show and hide functionality
+
+?>
+<div id="acftc-group-<?php echo $index; ?>" class="location-wrap">
+<?php 
+
+			if ( in_array( $location_rule['param'], self::$locations_tc_pro ) ) {
+
+				echo $this->get_locations_upgrade_notice_html( $location_rule );
+
+			} else {
+
+				$args = array(
+					'field_group_id' => $this->field_group_post_ID,
+					'location_rule_param' => $location_rule['param'] // included this time
+				);
+		
+				$parent_field_group = new ACFTC_Group( $args ); 
+
+				echo $parent_field_group->get_field_group_html(); 
+
+			}
+
+?>
+</div>
+<?php
+
+		return ob_get_clean();
+
+	}	
+
+
+	/**
+	 * Render upgrade link for location support
+	 * 
+	 * @param Array $location_rule
+	 * Array (
+	 *	[param] => block
+	 *	[operator] => ==
+	 *	[value] => acf/example
+	 * ) 
+	 * @return string 
+	 */
+	private function get_locations_upgrade_notice_html( $location_rule ) { 
+		
+		ob_start();
+
+?>
+	<div class="acftc-field-meta">
+		<span class="acftc-field-meta__title" data-pseudo-content="<?php echo $this->get_location_clean_text( $location_rule ); ?>"></span>
+	</div>
+
+	<div class="acftc-field-code">
+		<a href="#" class="acftc-field__copy acf-js-tooltip" title="Copy to Clipboard"></a>
+		<pre class="line-numbers"><code class="language-php"><?php
+		
+			echo htmlspecialchars( "<?php // Upgrade to ACF Theme Code Pro for '" . $location_rule['param'] . "' location support. ?>" ) . "\n";
+			echo htmlspecialchars( "<?php // Visit http://www.hookturn.io for more information. ?>" ) . "\n";
+			echo htmlspecialchars( "<?php // Alternatively add a Post or Page option to the Location Rules for this field group. ?>" ) . "\n";
+
+		?></code></pre>
+	</div>
+<?php
+
 		return ob_get_clean(); 
     }
 
 
 	/**
-	 * Render general upgrade link to appear below field group
+	 * Get HTML for any notices to be added below field group
 	 * 
 	 * @return string 
 	 */
-	private function get_after_field_group_upgrade_notice_html() { 
+	protected function get_after_field_group_notice_html() { 
 		
 		ob_start();
         ?>
