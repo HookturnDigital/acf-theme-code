ca798d193e094503d5422274a539b7fa2e7218bd
Ben Pearson <ben@benpearson.com.au>
2022-09-26 05:18:37 +1000

    Code link scroll: Add support for ACF 6

--- assets/acf-theme-code.js
+++ assets/acf-theme-code.js
@@ -4,6 +4,8 @@ const { __ } = wp.i18n; // Support for these functions can also be added: _x, _n
 (function($) {
 	$(document).ready(function() {
 
+		let $window = $(window);
+
 		// i18n strings
 		let i18nStrCopyAll = __('Copy all to clipboard', 'acf-theme-code');
 		let i18nStrCode = __('Code', 'acf-theme-code');
@@ -17,28 +19,15 @@ const { __ } = wp.i18n; // Support for these functions can also be added: _x, _n
 		$( "#acftc-meta-box .inside").prepend('<a href="#" class="acftc-copy-all acf-js-tooltip" title="' + i18nStrCopyAll + '"></a>');
 		$( "#acftc-meta-box .toggle-indicator").hide();
 
-		// ACF 4 - add anchor link to each field object
-		$( "div.field" ).each(function( index ) {
-			var field_key = $(this).attr("data-id");
-			var data_type = $(this).attr("data-type");
-
-			// find the parent class - this is to prevent mulitple links added to a repeater
-			var parent_class = $(this).parent().parent().prop('className');
-			if ( ( data_type != 'tab' ) && ( data_type != 'message') && ( parent_class == 'inside' ) ) {
-				  $(this).find('.row_options').first().append( '<span>| <a class="acftc-scroll__link" href="#acftc-' + field_key + '">' + i18nStrCode + '</a></span>' );
-			}
-
-		});
-
-		// ACF 5 - add anchor link to each field object
-		var fieldsV5 = $('#acf-field-group-fields .acf-field-object')
+		// Add anchor link to each field object 
+		var fields = $('#acf-field-group-fields .acf-field-object')
 
 			// exclude nested fields
 			.filter( function() {
 				return $(this).parentsUntil('#acf-field-group-fields', '.acf-field-object').length === 0;
 			});
 
-		fieldsV5.each(function( index ) {
+		fields.each(function( index ) {
 
 			var field_key = $(this).attr("data-id");
 			var data_type = $(this).attr("data-type");
@@ -52,12 +41,17 @@ const { __ } = wp.i18n; // Support for these functions can also be added: _x, _n
 
 		});
 
-		// smooth scroll - with offset for title and WP admin bar
+		// Scroll to the field code
 		$('a.acftc-scroll__link').click(function(event) {
 
 			// prevent default
 			event.preventDefault();
 
+			let wpAdminBar = $('#wpadminbar');
+			let acfAdminToolbar = $('.acf-admin-toolbar');
+			let acfHeaderBar = $('.acf-headerbar');
+			let scrollOffset = 44;
+			
 			// find the location that's selected
 			var location = $( "#acftc-group-option option:selected" ).val();
 
@@ -73,17 +67,26 @@ const { __ } = wp.i18n; // Support for these functions can also be added: _x, _n
 			var target = $('#' + location + ' ' + hash);
 
 			// find the offset from the top of our target field
-			var target_offset = target.offset().top;
+			var targetOffset = target.offset().top;
 
-			// after the large break point, the header is fixed
-			if (window.matchMedia("(max-width: 782px)").matches) {
-				var customoffset = 80; // increase offset for small screens
-			} else {
-				var customoffset = 60; // default offset for large screens
+			// WP admin bar: Include scroll offset if present and fixed or sticky
+			if ( wpAdminBar.length && ( wpAdminBar.css('position') === 'fixed' || wpAdminBar.css('position') === 'sticky' ) ) {
+				scrollOffset += wpAdminBar.outerHeight();
 			}
 
+			// ACF admin toolbar: Include scroll offset if present and fixed or sticky
+			if ( acfAdminToolbar.length && ( acfAdminToolbar.css('position') === 'fixed' || acfAdminToolbar.css('position') === 'sticky' ) ) {
+				scrollOffset += acfAdminToolbar.outerHeight();
+			}
+			
+			// ACF header bar: Include scroll offset if present and fixed or sticky
+			if ( acfHeaderBar.length && ( acfHeaderBar.css('position') === 'fixed' || acfHeaderBar.css('position') === 'sticky' ) ) {
+				scrollOffset += acfHeaderBar.outerHeight();
+			}
+
+			// Scroll to the target field
 			$('html,body').animate({
-				scrollTop: target_offset - customoffset
+				scrollTop: targetOffset - scrollOffset
 			}, 1000);
 
 			return false;
