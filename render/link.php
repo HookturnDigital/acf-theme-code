c3c39f965da956afd501926697027d6dc42e72dd
Ben Pearson <ben@benpearson.com.au>
2023-11-14 10:24:52 +1100

    Fix link field syntax spacing

--- render/link.php
+++ render/link.php
@@ -20,7 +20,7 @@ if ( $return_format == 'url' ) {
 
 	echo $this->indent . htmlspecialchars("<?php \$".$this->var_name." = " . $this->get_field_method . "( '" . $this->name ."'". $this->location_rendered_param . " ); ?>")."\n";
 	echo $this->indent . htmlspecialchars("<?php if ( \$".$this->var_name." ) : ?>")."\n";
-	echo $this->indent . htmlspecialchars("	<a href=\"<?php echo esc_url( \$" . $this->var_name . ") ; ?>\"><?php echo esc_html( \$" . $this->var_name . " ); ?></a>")."\n";
+	echo $this->indent . htmlspecialchars("	<a href=\"<?php echo esc_url( \$" . $this->var_name . "); ?>\"><?php echo esc_html( \$" . $this->var_name . " ); ?></a>")."\n";
 	echo $this->indent . htmlspecialchars("<?php endif; ?>\n");
 
 }
